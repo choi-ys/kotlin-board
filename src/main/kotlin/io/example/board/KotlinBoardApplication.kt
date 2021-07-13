a0c5a0bca9a90abc1c699acfb331d9b5cd360fec
choi-ys <project.log.062@gmail.com>
2021-07-13 23:50:36 +0900

    [2021-07-13]용석:Initial commit

---
+++ src/main/kotlin/io/example/board/KotlinBoardApplication.kt
@@ -0,0 +1,11 @@
+package io.example.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBoardApplication>(*args)
+}
---
+++ src/test/kotlin/io/example/board/KotlinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.example.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
