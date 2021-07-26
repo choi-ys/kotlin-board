83a193efb88600b426af95c84f2e390f40ba14d4
choi-ys <project.log.062@gmail.com>
2021-07-26 23:51:05 +0900

    [2021-07-26]용석:application 설정 변경

--- src/main/kotlin/io/example/board/config/application/ApplicationConfig.kt
+++ src/main/kotlin/io/example/board/config/application/ApplicationConfig.kt
@@ -2,11 +2,13 @@ package io.example.board.config.application
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class ApplicationConfig {
 
     @Bean
-    fun passwordEncoder() = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 }
