22748833bdbe414a01ea2b4b59fa8f6d6e54575c
choi-ys <project.log.062@gmail.com>
2021-07-28 06:26:12 +0900

    [2021-07-28]용석:회원가입 API 및 TC 추가

---
+++ src/main/kotlin/io/example/board/controller/MemberController.kt
@@ -0,0 +1,26 @@
+package io.example.board.controller
+
+import io.example.board.domain.dto.request.SignupRequest
+import io.example.board.service.MemberService
+import mu.KotlinLogging
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+private val logger = KotlinLogging.logger {  }
+
+@RestController
+@RequestMapping(
+    value = ["member"],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+)
+class MemberController(
+    private val memberService: MemberService
+) {
+
+    @PostMapping("signup")
+    fun signup(@RequestBody signupRequest: SignupRequest) : ResponseEntity<*>{
+        return ResponseEntity.ok(memberService.signup(signupRequest))
+    }
+}
---
+++ src/test/kotlin/io/example/board/controller/MemberControllerTest.kt
@@ -0,0 +1,61 @@
+package io.example.board.controller
+
+import io.example.board.config.test.WebMvcTestConfig
+import io.example.board.domain.dto.response.SignupResponse
+import io.example.board.service.MemberService
+import io.example.board.util.generator.MemberGenerator
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@DisplayName("Controller:Member")
+@WebMvcTest(MemberController::class)
+internal class MemberControllerTest : WebMvcTestConfig(){
+
+    @MockBean
+    lateinit var memberService: MemberService
+
+    private val SIGNUP_URL = "/member/signup"
+    private val signupRequest = MemberGenerator.generateSignupRequest()
+    private val signupResponse = SignupResponse(id = 1L, name = signupRequest.name, email = signupRequest.email, nickname = signupRequest.nickname)
+
+    @Test
+    @DisplayName("API:회원 가입")
+    @Throws(Exception::class)
+    fun signup(){
+        // Given
+        given(memberService.signup(signupRequest)).willReturn(signupResponse)
+
+        // When
+        val resultAction = this.post(SIGNUP_URL, signupRequest)
+
+        // Then
+        resultAction.andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("id").value(signupResponse.id))
+            .andExpect(jsonPath("name").value(signupResponse.name))
+            .andExpect(jsonPath("email").value(signupResponse.email))
+            .andExpect(jsonPath("nickname").value(signupResponse.nickname))
+    }
+
+    @Test
+    @DisplayName("API:회원 가입 실패(값이 옳바르지 않은 요청)")
+    fun signup_Fail_CauseInvalidArgument(){
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    @DisplayName("API:회원 가입 실패(유효하지 못한 요청)")
+    fun signup_Fail_CauseIllegalArgument(){
+
+    }
+}
