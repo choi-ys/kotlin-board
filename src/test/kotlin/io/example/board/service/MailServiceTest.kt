508c0e8022a1a89930722bc5b8f3a1747b1e2bc7
choi-ys <project.log.062@gmail.com>
2021-07-27 16:27:34 +0900

    [2021-07-27]용석:메일 전송 TC 수정

--- src/test/kotlin/io/example/board/service/MailServiceTest.kt
+++ src/test/kotlin/io/example/board/service/MailServiceTest.kt
@@ -1,24 +1,45 @@
 package io.example.board.service
 
-import io.example.board.config.base.BaseTestAnnotations
-import org.junit.jupiter.api.Disabled
+import io.example.board.config.test.MockingTestAnnotations
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.*
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.doAnswer
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 
-@SpringBootTest
 @DisplayName("Service:Mail")
-internal class MailServiceTest : BaseTestAnnotations(){
+internal class MailServiceTest : MockingTestAnnotations(){
 
-    @Autowired
+    @Mock
+    lateinit var javaMailSender: JavaMailSender
+
+    @InjectMocks
     lateinit var mailService: MailService
 
     @Test
     @DisplayName("회원 인증 메일 전송")
-    @Disabled
     fun send(){
+        // Given
         var to = "rcn115@naver.com"
-        mailService.sendCertificationMail(to)
+        val simpleMailMessage = SimpleMailMessage().also {
+            it.setTo(to)
+            it.setSubject("test title")
+            it.setText("test content")
+        }
+
+//        doAnswer(`when`(javaMailSender.send(simpleMailMessage))).
+            doNothing().`when`(javaMailSender).send(any(SimpleMailMessage::class.java))
+//            doNothing().`when`(javaMailSender)
+
+        // When
+        val sendCertificationMail = mailService.sendCertificationMail(to)
+
+        // Then
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage::class.java))
     }
 }
