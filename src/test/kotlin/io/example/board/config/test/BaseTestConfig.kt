093bc5711ef94f1cccc4f7fc41ec3b760359fd77
choi-ys <project.log.062@gmail.com>
2021-08-30 05:56:28 +0900

    [2021-08-30]용석:p6spy를 이용한 실행 출력 설정에 따른 TC 설정부 수정

--- src/test/kotlin/io/example/board/config/test/BaseTestConfig.kt
+++ src/test/kotlin/io/example/board/config/test/BaseTestConfig.kt
@@ -1,11 +1,9 @@
 package io.example.board.config.test
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.MethodName::class)
-@Disabled
 open class BaseTestConfig
--- src/test/kotlin/io/example/board/config/test/JpaTestConfig.kt
+++ src/test/kotlin/io/example/board/config/test/JpaTestConfig.kt
@@ -1,14 +1,18 @@
 package io.example.board.config.test
 
+import com.github.gavlyukovskiy.boot.jdbc.decorator.DataSourceDecoratorAutoConfiguration
 import io.example.board.config.application.DataJpaAuditorConfig
+import io.example.board.config.p6spy.P6spyLogMessageFormatConfiguration
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
 import javax.persistence.EntityManager
 
-@DataJpaTest
-@Import(DataJpaAuditorConfig::class)
-class JpaTestConfig : BaseTestConfig() {
+@DataJpaTest(showSql = false)
+@ImportAutoConfiguration(DataSourceDecoratorAutoConfiguration::class)
+@Import(DataJpaAuditorConfig::class, P6spyLogMessageFormatConfiguration::class)
+class JpaTestConfig : BaseTestConfig(){
 
     @Autowired
     lateinit var entityManager: EntityManager
