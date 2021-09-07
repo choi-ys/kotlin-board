8686e66f7dd676d17c74f92b3d840ab8d3b182d3
choy-ys <project.log.062@gmail.com>
2021-09-07 20:23:51 +0900

    [2021-09-07]용석:JWT를 이용한 인증처리 #6 JWT 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail") // mail-sender
     implementation("org.springframework.boot:spring-boot-starter-security") // spring-security
 
+    implementation("com.auth0:java-jwt:3.16.0") // oauth0
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.7.0") // p6spy
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
 
