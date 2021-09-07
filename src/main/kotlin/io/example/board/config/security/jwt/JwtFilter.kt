cc2cbbc9622b5290ea2bb190628d43ea380f0bff
choy-ys <project.log.062@gmail.com>
2021-09-07 20:22:05 +0900

    [2021-09-07]용석:JWT를 이용한 인증처리 #4 JWT 검증 Filter 작성

---
+++ src/main/kotlin/io/example/board/config/security/jwt/JwtFilter.kt
@@ -0,0 +1,28 @@
+package io.example.board.config.security.jwt
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.GenericFilterBean
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class JwtFilter(
+    private val tokenUtils: TokenUtils
+) : GenericFilterBean() {
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val token = tokenUtils.resolve(request as HttpServletRequest)
+
+        val verifyResult = tokenUtils.verify(token)
+        if (verifyResult.success) {
+            SecurityContextHolder.getContext().authentication = token?.let {
+                tokenUtils.getAuthentication(it)
+            }
+        }
+        chain.doFilter(request, response)
+        return
+    }
+}
