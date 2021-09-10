709ccb2a703a79da20c774b98abcd9aa7e1bfd67
choy-ys <project.log.062@gmail.com>
2021-09-10 20:11:40 +0900

    [2021-09-10]용석: 통합 Test 관련 설정 수정
     - MockMvc를 이용한 API Test시 ServletRequest의 한글 설정 부 Annotations으로 대체
     - 중복 선언된 어노테이션 설정 부로 추출

--- src/test/kotlin/io/example/board/config/test/IntegrationTestConfig.kt
+++ src/test/kotlin/io/example/board/config/test/IntegrationTestConfig.kt
@@ -2,6 +2,8 @@ package io.example.board.config.test
 
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@Transactional
 class IntegrationTestConfig : MockMvcTestConfig()
--- src/test/kotlin/io/example/board/config/test/MockMvcTestConfig.kt
+++ src/test/kotlin/io/example/board/config/test/MockMvcTestConfig.kt
@@ -1,22 +1,14 @@
 package io.example.board.config.test
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.util.MultiValueMap
-import org.springframework.web.context.WebApplicationContext
-import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
 
-@AutoConfigureMockMvc
+@EnableMockMvc
 open class MockMvcTestConfig : BaseTestConfig() {
 
     @Autowired
@@ -24,20 +16,6 @@ open class MockMvcTestConfig : BaseTestConfig() {
 
     @Autowired
     lateinit var objectMapper: ObjectMapper
-
-    @Autowired
-    lateinit var webApplicationContext: WebApplicationContext
-
-    @BeforeEach
-    fun setUp() {
-        mockMvc =
-            MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .addFilter<DefaultMockMvcBuilder>({ request: ServletRequest?, response: ServletResponse, chain: FilterChain ->
-                    request?.characterEncoding = "UTF-8"
-                    response.characterEncoding = "UTF-8"
-                    chain.doFilter(request, response)
-                }).build()
-    }
 
     @Throws(Exception::class)
     fun get(requestUrl: String): ResultActions {
