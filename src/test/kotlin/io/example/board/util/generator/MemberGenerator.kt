050180f475d87225c3a4db37b08aa6a95fdb7210
choy-ys <project.log.062@gmail.com>
2021-09-11 03:16:11 +0900

    [2021-09-11]용석: TC 수행을 위한 저장된 Member Entity 반환 부 추가

--- src/test/kotlin/io/example/board/util/generator/MemberGenerator.kt
+++ src/test/kotlin/io/example/board/util/generator/MemberGenerator.kt
@@ -2,12 +2,21 @@ package io.example.board.util.generator
 
 import io.example.board.domain.dto.request.SignupRequest
 import io.example.board.domain.entity.rdb.member.Member
+import io.example.board.repository.MemberRepository
 import org.junit.jupiter.api.Disabled
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestComponent
 
 @Disabled
 @TestComponent
 class MemberGenerator {
+
+    @Autowired
+    lateinit var memberRepository: MemberRepository
+
+    fun savedMember(): Member {
+        return memberRepository.saveAndFlush(MemberGenerator.generateMemberEntity())
+    }
 
     companion object {
         var name: String = "최용석"
