4ebfe3cede3a2976f7b913bd935b7894ed4cbe61
choy-ys <project.log.062@gmail.com>
2021-09-11 03:15:25 +0900

    [2021-09-10]용석: Member Entity의 이력관리를 위한 History Entity 처리 부 삭제
     - TO-BE : Kafka를 이용한 Event 기반 아키텍쳐를 이용한 이력 관리

--- src/main/kotlin/io/example/board/domain/entity/rdb/listener/HistoryListener.kt
+++ src/main/kotlin/io/example/board/domain/entity/rdb/listener/HistoryListener.kt
@@ -6,6 +6,7 @@ import io.example.board.domain.entity.rdb.common.EventType
  * @author : choi-ys
  * @date : 2021-08-29 오후 11:50
  */
+// TODO 해당 이벤트 발생 시, Kafka를 이용한 Event 기반 아키텍쳐로 이력 관리
 interface HistoryListener {
     fun postPersist(entity: Any)
     fun postUpdate(entity: Any)
