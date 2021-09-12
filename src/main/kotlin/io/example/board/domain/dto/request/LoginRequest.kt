e240b49c1762a031d1b12520530ee8deee26c31c
choy-ys <project.log.062@gmail.com>
2021-09-13 02:38:55 +0900

    [2021-09-13]용석:로그인 요청 객체에 JSR 380 Annotations 적용

--- src/main/kotlin/io/example/board/domain/dto/request/LoginRequest.kt
+++ src/main/kotlin/io/example/board/domain/dto/request/LoginRequest.kt
@@ -1,6 +1,11 @@
 package io.example.board.domain.dto.request
 
+import javax.validation.constraints.NotBlank
+
 data class LoginRequest(
+    @NotBlank(message = "이메일을 입력해주세요.")
     val email: String,
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     val password: String
 )
