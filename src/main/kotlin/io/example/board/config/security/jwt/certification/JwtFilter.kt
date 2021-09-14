04929131ef4a21632cdfcd7034c81ead72beda2c
choy-ys <project.log.062@gmail.com>
2021-09-15 00:48:21 +0900

    [2021-09-15]용석:JWT 검증 Filter 코드 및 분기 조건 일부 수정

--- src/main/kotlin/io/example/board/config/security/jwt/certification/JwtFilter.kt
+++ src/main/kotlin/io/example/board/config/security/jwt/certification/JwtFilter.kt
@@ -1,23 +1,21 @@
 package io.example.board.config.security.jwt.certification
 
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.GenericFilterBean
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 
-@Component
 class JwtFilter(
-    private val tokenUtils: TokenUtils
+    private val tokenUtils: TokenUtils,
 ) : GenericFilterBean() {
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val token = tokenUtils.resolve(request as HttpServletRequest)
         if ("" != token) {
             val verifyResult = tokenUtils.verify(token)
-            if (verifyResult.success) {
+            if (verifyResult?.success ?: false) {
                 val authentication = tokenUtils.getAuthentication(token)
                 SecurityContextHolder.getContext().authentication = authentication
             }
