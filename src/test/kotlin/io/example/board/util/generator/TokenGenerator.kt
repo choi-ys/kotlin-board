d130108e83192e864874388b81956d63a08fd9f3
choy-ys <project.log.062@gmail.com>
2021-09-14 16:10:29 +0900

    [2021-09-14]용석:TC 수행을 위한 인증 토큰 생성 부 수정
     - 불필요 로직 개선

--- src/test/kotlin/io/example/board/util/generator/TokenGenerator.kt
+++ src/test/kotlin/io/example/board/util/generator/TokenGenerator.kt
@@ -1,32 +1,36 @@
 package io.example.board.util.generator
 
-import io.example.board.domain.dto.request.LoginRequest
+import io.example.board.config.security.jwt.offer.TokenProvider
+import io.example.board.domain.entity.rdb.member.Member
+import io.example.board.domain.vo.login.LoginUserAdapter
 import io.example.board.domain.vo.login.token.Token
-import io.example.board.service.LoginService
-import io.example.board.service.MemberService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestComponent
+import org.springframework.context.annotation.Import
 
 /**
  * @author : choi-ys
  * @date : 2021/09/10 2:38 오후
  */
 @TestComponent
+@Import(MemberGenerator::class)
 class TokenGenerator {
 
     @Autowired
-    private lateinit var memberService: MemberService
+    private lateinit var memberGenerator: MemberGenerator
 
     @Autowired
-    private lateinit var loginService: LoginService
+    private lateinit var tokenProvider: TokenProvider
 
     fun generateToken(): Token {
-        val signupRequest = MemberGenerator.signupRequest()
-        memberService.signup(signupRequest)
+        val savedMember = memberGenerator.savedMember()
+        val loginUserAdapter = LoginUserAdapter(savedMember.email, savedMember.mapToSimpleGrantedAuthority())
+        return tokenProvider.createToken(loginUserAdapter)
+    }
 
-        val loginRequest = LoginRequest(signupRequest.email, MemberGenerator.password)
-
-        return loginService.login(loginRequest).token
+    fun generateToken(savedMember: Member): Token {
+        val loginUserAdapter = LoginUserAdapter(savedMember.email, savedMember.mapToSimpleGrantedAuthority())
+        return tokenProvider.createToken(loginUserAdapter)
     }
 
     companion object {
