7822d55dd7f95e6230c285c0def011b6bb94a44d
choy-ys <project.log.062@gmail.com>
2021-09-14 18:33:05 +0900

    [2021-09-14]용석:게시글 생성 API 구현 및 TC 작성

---
+++ src/main/kotlin/io/example/board/controller/PostController.kt
@@ -0,0 +1,27 @@
+package io.example.board.controller
+
+import io.example.board.domain.dto.request.PostRequest
+import io.example.board.domain.vo.login.CurrentUser
+import io.example.board.domain.vo.login.LoginUser
+import io.example.board.service.PostService
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+/**
+ * @author : choi-ys
+ * @date : 2021/09/13 2:32 오후
+ */
+@RestController
+@RequestMapping(
+    value = ["post"],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+)
+class PostController(private val postService: PostService) {
+
+    @PostMapping
+    fun post(@Valid @RequestBody postRequest: PostRequest, @CurrentUser loginUser: LoginUser) =
+        ResponseEntity.ok(postService.post(postRequest, loginUser))
+}
---
+++ src/test/kotlin/io/example/board/controller/PostControllerTest.kt
@@ -0,0 +1,76 @@
+package io.example.board.controller
+
+import io.example.board.config.test.IntegrationTestConfig
+import io.example.board.domain.dto.request.PostRequest
+import io.example.board.util.generator.MemberGenerator
+import io.example.board.util.generator.PostGenerator
+import io.example.board.util.generator.TokenGenerator
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+/**
+ * @author : choi-ys
+ * @date : 2021/09/13 2:35 오후
+ */
+@DisplayName("Controller:Post")
+@Import(TokenGenerator::class, PostGenerator::class, MemberGenerator::class)
+internal class PostControllerTest : IntegrationTestConfig() {
+
+    @Autowired
+    lateinit var tokenGenerator: TokenGenerator
+
+    @Autowired
+    lateinit var postGenerator: PostGenerator
+
+    @Autowired
+    lateinit var memberGenerator: MemberGenerator
+
+    private val POST_URL = "/post"
+
+    @Test
+    @DisplayName("API:[200]게시글 생성")
+    fun post() {
+        // Given
+        val title = "게시글 제목"
+        val content = "게시글 본문"
+        val postRequest = PostRequest(title, content)
+
+        val bearerToken = TokenGenerator.makeBearerToken(tokenGenerator.generateToken().accessToken)
+
+        // When
+        val resultActions = this.mockMvc.perform(
+            MockMvcRequestBuilders.post(POST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION, bearerToken)
+                .content(this.objectMapper.writeValueAsBytes(postRequest))
+        )
+
+        // Then
+        resultActions.andDo(print())
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    @DisplayName("API:[403]게시글 생성")
+    fun post_Fail_CauseNoCredentials() {
+        // Given
+        val title = "게시글 제목"
+        val content = "게시글 본문"
+        val postRequest = PostRequest(title, content)
+
+        // When
+        val resultActions = this.post(POST_URL, postRequest)
+
+        // Then
+        resultActions.andDo(print())
+            .andExpect(status().isForbidden)
+    }
+}
