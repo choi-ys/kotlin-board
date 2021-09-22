26e221acf3400b649ce225db76a77a0339398097
choy-ys <project.log.062@gmail.com>
2021-09-22 20:04:29 +0900

    [2021-09-22]용석:TC Comment 오타 수정

--- src/test/kotlin/io/example/board/config/security/jwt/TokenProviderTest.kt
+++ src/test/kotlin/io/example/board/config/security/jwt/TokenProviderTest.kt
@@ -21,7 +21,7 @@ import java.time.format.DateTimeFormatter
  * @date : 2021/09/09 12:13 오후
  */
 @SpringBootTest(classes = [TokenProvider::class, TokenUtils::class])
-@DisplayName("Config:TokenProvier")
+@DisplayName("Config:TokenProvider")
 internal class TokenProviderTest {
 
     @Autowired
